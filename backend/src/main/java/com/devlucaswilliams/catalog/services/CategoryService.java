8a1e954a38eb3cfc96c18563c3f5c9667598a674
Lucas williams <lucaswilliamscesar@gmail.com>
2024-06-29 09:14:02 -0300

    create method Category findById

--- backend/src/main/java/com/devlucaswilliams/catalog/services/CategoryService.java
+++ backend/src/main/java/com/devlucaswilliams/catalog/services/CategoryService.java
@@ -1,6 +1,7 @@
 package com.devlucaswilliams.catalog.services;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,14 +14,21 @@ import com.devlucaswilliams.catalog.repositories.CategoryRepository;
 
 @Service
 public class CategoryService {
-	
+
 	@Autowired
 	private CategoryRepository categoryRepository;
-	
+
 	@Transactional(readOnly = true)
-	public List<CategoryDTO> findAll(){
+	public List<CategoryDTO> findAll() {
 		List<Category> list = categoryRepository.findAll();
 		return list.stream().map(x -> new CategoryDTO(x)).collect(Collectors.toList());
+	}
+
+	@Transactional(readOnly = true)
+	public CategoryDTO findById(Long id) {
+		Optional<Category> obj = categoryRepository.findById(id);
+		Category entity = obj.get();
+		return new CategoryDTO(entity);
 	}
 
 }
