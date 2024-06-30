f84304e60b52046d544e1e143aec233631932230
Lucas williams <lucaswilliamscesar@gmail.com>
2024-06-30 06:55:50 -0300

    method insert

--- backend/src/main/java/com/devlucaswilliams/catalog/resources/CategoryResource.java
+++ backend/src/main/java/com/devlucaswilliams/catalog/resources/CategoryResource.java
@@ -1,5 +1,6 @@
 package com.devlucaswilliams.catalog.resources;
 
+import java.net.URI;
 import java.util.List;
 
 
@@ -7,8 +8,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.devlucaswilliams.catalog.dto.CategoryDTO;
 import com.devlucaswilliams.catalog.services.CategoryService;
@@ -30,6 +34,14 @@ public class CategoryResource {
 	public ResponseEntity<CategoryDTO> findById(@PathVariable Long id){
 		CategoryDTO dto = categoryService.findById(id);
 		return ResponseEntity.ok().body(dto);
+	}
+	
+	@PostMapping
+	public ResponseEntity<CategoryDTO> insert(@RequestBody CategoryDTO dto){
+		dto = categoryService.insert(dto);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+				.buildAndExpand(dto.getName()).toUri();
+		return ResponseEntity.created(uri).body(dto);
 	}
 	
 }
