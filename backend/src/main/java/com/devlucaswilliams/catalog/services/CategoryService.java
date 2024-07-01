bfb7f8cbacce916e95e4d5c33201bb78c515fb89
Lucas williams <lucaswilliamscesar@gmail.com>
2024-06-30 22:46:52 -0300

    pagination

--- backend/src/main/java/com/devlucaswilliams/catalog/services/CategoryService.java
+++ backend/src/main/java/com/devlucaswilliams/catalog/services/CategoryService.java
@@ -1,12 +1,11 @@
 package com.devlucaswilliams.catalog.services;
 
-import java.util.List;
-
 import java.util.Optional;
-import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
@@ -26,9 +25,9 @@ public class CategoryService {
 	private CategoryRepository categoryRepository;
 
 	@Transactional(readOnly = true)
-	public List<CategoryDTO> findAll() {
-		List<Category> list = categoryRepository.findAll();
-		return list.stream().map(x -> new CategoryDTO(x)).collect(Collectors.toList());
+	public Page<CategoryDTO> findAll(PageRequest pageRequest) {
+		Page<Category> page = categoryRepository.findAll(pageRequest);
+		return page.map(x -> new CategoryDTO(x));
 	}
 
 	@Transactional(readOnly = true)
